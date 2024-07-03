4ca302f056fc9c52caf880fe8da276cb83cf18d4
Nao Sakanoue <nsakanou@c1r6s7.42tokyo.jp>
2024-07-03 12:39:40 +0900

    03

--- ex03/vendor/piscine/index.go
+++ ex03/vendor/piscine/index.go
@@ -1,9 +1,18 @@
 package piscine
 
+func Substring(s string, start int, len int) string {
+	return s[start : start + len]
+}
+
 func Index(s string, toFind string) int {
-	if s != "" || toFind != "" {
-		for i := 0; i <= len(s) - len(toFind); i++ {
-			if 
+	if len(toFind) == 0 {
+		return 0
+	}
+	for i := 0; i <= len(s) - len(toFind); i++ {
+		substr := Substring(s, i, len(toFind))
+		if substr == toFind {
+			return i
 		}
 	}
+	return -1
 }
